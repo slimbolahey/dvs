88d8204b01f7b5c1fbc9b9bded0dd74634ac57eb
slim <108889694+slimbolahey@users.noreply.github.com>
2023-12-27 19:38:54 -0500

    Update client.lua
    Optimization fixes

--- client.lua
+++ client.lua
@@ -1,12 +1,38 @@
-local ped
+local pedInVehicle = false
 
-Citizen.CreateThread( function()
-	while true do
-		Wait(1)
-		ped = GetPlayerPed(-1)
+-- Function to set high-speed bump severity multiplier
+local function SetBumpMultiplier()
+    SetCarHighSpeedBumpSeverityMultiplier(0, 0)
+end
 
-		if IsPedInAnyVehicle(ped) then
-			SetCarHighSpeedBumpSeverityMultiplier(0,0)
-		end
-	end
+-- Event handler for when the player enters a vehicle
+RegisterNetEvent('onClientVehicleEnter')
+AddEventHandler('onClientVehicleEnter', function()
+    pedInVehicle = true
+    SetBumpMultiplier()
+end)
+
+-- Event handler for when the player exits a vehicle
+RegisterNetEvent('onClientVehicleExit')
+AddEventHandler('onClientVehicleExit', function()
+    pedInVehicle = false
+end)
+
+-- Main loop to check if the player is in a vehicle
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)  -- Yield the thread
+
+        local ped = GetPlayerPed(-1)
+
+        if DoesEntityExist(ped) and not IsEntityDead(ped) then
+            local isInVehicle = IsPedInAnyVehicle(ped, false)
+            
+            if isInVehicle and not pedInVehicle then
+                TriggerEvent('onClientVehicleEnter')
+            elseif not isInVehicle and pedInVehicle then
+                TriggerEvent('onClientVehicleExit')
+            end
+        end
+    end
 end)
