224adcf41e3e6a10774c618b758ba9c0f151e7b4
slim <108889694+slimbolahey@users.noreply.github.com>
2024-01-02 20:46:39 -0500

    Update client.lua
    Last one.

--- client.lua
+++ client.lua
@@ -3,19 +3,12 @@ local function SetShakeMultiplier()
 end
 
 Citizen.CreateThread(function()
-    local wasInVehicle = false
-
     while true do
         Citizen.Wait(500)
 
         local ped = GetPlayerPed(-1)
-        local isInVehicle = DoesEntityExist(ped) and not IsEntityDead(ped) and IsPedInAnyVehicle(ped, false)
-
-        if isInVehicle and not wasInVehicle then
+        if IsPedInAnyVehicle(ped, false) then
             SetShakeMultiplier()
-            wasInVehicle = true
-        elseif not isInVehicle and wasInVehicle then
-            wasInVehicle = false
         end
     end
 end)
